beb516e26303c79e59ec5c4e7203ef3779e8e49a
Afif <49422146+Avwaveaf@users.noreply.github.com>
2025-02-03 15:08:20 +0700

    Initial Commit

---
+++ scrumdinger/scrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  scrumdingerApp.swift
+//  scrumdinger
+//
+//  Created by Afif Fadillah on 03/02/25.
+//
+
+import SwiftUI
+
+@main
+struct scrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ scrumdingerTests/scrumdingerTests.swift
@@ -0,0 +1,17 @@
+//
+//  scrumdingerTests.swift
+//  scrumdingerTests
+//
+//  Created by Afif Fadillah on 03/02/25.
+//
+
+import Testing
+@testable import scrumdinger
+
+struct scrumdingerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
